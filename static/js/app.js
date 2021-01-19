9ae55dfdc5d9185959143f152ab56f64a7f8bb79
Nick F <ngf8@nau.edu>
2021-01-18 20:03:36 -0700

    finished level 1

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,49 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+tableData.forEach((SpookyVisit) => {
+    var row = tbody.append("tr");
+    Object.entries(SpookyVisit).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+
+
+var form = d3.select("form");
+
+form.on("submit", runEnter);
+
+function buildTable(data){
+            tbody.html("");
+            data.forEach((item) => {
+                var newRow=tbody.append('tr');
+                Object.values(item).forEach((val)=> {
+                    newRow.append('td').text(val)
+                });
+            });
+        };
+
+
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter((data) => data.datetime === inputValue);
+    filteredData.forEach((SpookyVisit) => {
+        var row = tbody.append("tr");
+        Object.entries(SpookyVisit).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+      console.log(filteredData);
+      buildTable(filteredData);     
+};
+
